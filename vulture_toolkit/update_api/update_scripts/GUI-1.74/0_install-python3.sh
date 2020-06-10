374074071e777f8f86bbf26e85b9623d40dfd7cf
KGuillemot <kevin.guillemot2@gmail.com>
2020-06-10 16:40:42 +0200

    1.74 : Remove openmp in update script to prevent crash

--- vulture_toolkit/update_api/update_scripts/GUI-1.74/0_install-python3.sh
+++ vulture_toolkit/update_api/update_scripts/GUI-1.74/0_install-python3.sh
@@ -18,6 +18,6 @@ cd /tmp
 /usr/sbin/pkg remove -y py27-setuptools py27-sqlite3 py27-kerberos
 
 /bin/echo -n "[+] Installing Python3 packages ..."
-/usr/sbin/pkg install -y python37 py37-setuptools py37-sqlite3 py37-kerberos openmp && echo "OK" || (echo "KO" && echo "[!] Please install following packages manually : python37 py37-setuptools py37-sqlite3 py37-kerberos openmp")
+/usr/sbin/pkg install -y python37 py37-setuptools py37-sqlite3 py37-kerberos && echo "OK" || (echo "KO" && echo "[!] Please install following packages manually : python37 py37-setuptools py37-sqlite3 py37-kerberos")
 
 /bin/echo "[*] Upgrade of Python3 packages done."
