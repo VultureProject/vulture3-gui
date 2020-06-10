b15307237554f603a1ba144c647f3100806e2415
KGuillemot <kevin.guillemot2@gmail.com>
2020-06-10 16:29:46 +0200

    1.76 : Reload Rsyslog config during update

---
+++ vulture_toolkit/update_api/update_scripts/GUI-1.76/reload_rsyslogd_conf.py
@@ -0,0 +1,42 @@
+#!/home/vlt-gui/env/bin/python2.7
+# coding:utf-8
+
+"""This file is part of Vulture 3.
+
+Vulture 3 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Vulture 3 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Vulture 3.  If not, see http://www.gnu.org/licenses/.
+"""
+__author__ = "Kevin Guillemot"
+__credits__ = []
+__license__ = "GPLv3"
+__version__ = "3.0.0"
+__maintainer__ = "Vulture Project"
+__email__ = "contact@vultureproject.org"
+__doc__ = """This migration script reload Rsyslog configuration"""
+
+import os
+import sys
+
+sys.path.append('/home/vlt-gui/vulture')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'vulture.settings')
+
+import django
+django.setup()
+
+from gui.signals.gui_signals import log_modified
+
+if __name__ == '__main__':
+
+    log_modified.send(sender=None)
+
+    print("Rsyslogd configuration reloaded.")
